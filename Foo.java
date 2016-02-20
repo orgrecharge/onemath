234e577b50f9b79967f8845cf038602450a5f1b7
Eton Dolittle <etondolittle@gmail.com>
2016-02-20 15:24:03 -0800

    added apache math

--- Foo.java
+++ Foo.java
@@ -1,11 +1,13 @@
+import org.apache.commons.math3.util.FastMath;
+
 public class Foo {
 	public static void main(String[] arg) {
 		Foo f = new Foo();
-		int length = f.getPositive();
-		System.out.println("Foo: length="+length);	
+		double tan = f.getTan();
+		System.out.println("Foo: tan="+tan);
 	}
-	
-	public int getPositive() {
-		return 10;
+
+	public double getTan() {
+		return FastMath.tan(10);
 	}
 }
