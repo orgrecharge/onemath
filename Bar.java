25922ca70e175e3049ef3b941f3c01579276ef5d
Eton Dolittle <etondolittle@gmail.com>
2016-02-20 14:36:08 -0800

    added bar

---
+++ Bar.java
@@ -0,0 +1,11 @@
+public class Bar {
+	public static void main(String[] arg) {
+		Bar f = new Bar();
+		int length = f.getPositive();
+		System.out.println("Bar: length="+length);	
+	}
+	
+	public int getPositive() {
+		return 10;
+	}
+}
